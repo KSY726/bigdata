dd6af23dee20426010030e9d3ead9fdc765d9c7d
ksy726 <syku0903@gmail.com>
2024-09-30 12:06:54 +0900

    멜론 차트 제목 1개 가져오기

--- randtest.py
+++ randtest.py
@@ -10,20 +10,24 @@ print(random.random())
 menu = '치킨', '피자', '분식', '중식'
 print(random.choice(menu))
 
-import requests # type: ignore
-from bs4 import BeautifulSoup # type: ignore
- 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('스크래핑할 주소',headers=headers)
- 
-soup = BeautifulSoup(data.text, 'html.parser')
+import requests
+from bs4 import BeautifulSoup
 
-import requests # type: ignore
-from bs4 import BeautifulSoup # type: ignore
- 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.melon.com/chart/',headers=headers)
- 
+
 soup = BeautifulSoup(data.text, 'html.parser')
- 
-print(soup)
+
+top_100 = soup.select('#lst50 > td > div') #tr로 이루어진 list
+
+for tr in top_100 :
+    rank = tr.select_one('span.rank')
+        
+    if rank is not None:
+        print(rank.text, end=". ")
+        
+    title = tr.select_one('div > div.ellipsis.rank01 > span > a')
+    
+    if title is not None:
+        artist = tr.select_one('div.ellipsis.rank02 > a').text
+        print(artist,'-', title.text)
